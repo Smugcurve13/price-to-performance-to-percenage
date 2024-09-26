70205335d6e015b25b0490c2210d4c085137a1bb
Smugcurve13 <sambhavsoni14@gmail.com>
2024-09-27 04:37:12 +0530

    added input validation, checks if input is +ve and a number

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-# pc1_price = int(input("enter the price of pc1: "))
-# pc2_price = int(input("enter the price of pc2: "))
+# def is_valid_input(value):
+#     # check if the value is an positive integer
+#     return isinstance(value, int) and value > 0
 
-# pc1_performance = int(input("Enter performace metric of pc1: "))
-# pc2_performance = int(input("Enter performace metric of pc2: "))
+
+# def get_valid_input(prompt):
+#     while True:
+#         try:
+#             price = int(input(prompt))
+#             if is_valid_input(price):
+#                 return price
+#             else:
+#                 print("Please enter a valid positive integer")
+#         except ValueError:
+#             print("Invalid inout , please enter a number.")
+
+
+
+# pc1_price = get_valid_input("enter the price of pc1: ")
+# pc2_price = get_valid_input("enter the price of pc2: ")
+
+# pc1_performance = get_valid_input("Enter performace metric of pc1: ")
+# pc2_performance = get_valid_input("Enter performace metric of pc2: ")
+
 pc1_name = "Intel Core i9-14900F"
 pc2_name = "Intel Core i9-13900KF"
 
