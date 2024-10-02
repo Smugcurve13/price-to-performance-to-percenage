829656b08e1838168d6146fc98e6e501f4fdbaa0
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-02 21:36:01 +0530

    added tables under after entering pc details

--- new-features/feature.py
+++ new-features/feature.py
@@ -1,20 +1,34 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Price to Performance to Percentage")
-st.write("This Tool provide you how much performance your dollars net you")
+st.write("This Tool provide you how much performance your dollars will net you")
 
 num_of_pcs = st.text_input(label="how many pcs you want to compare?")
 try:
     num_of_pcs = int(num_of_pcs)
+    name=[]
+    price=[]
+    performance=[]
+
+    pcs_dict = {}
+
     for num in range(1,num_of_pcs+1):
         name = st.text_input(label=f"enter name of PC {num}")
         price = int(st.text_input(label=f"enter price of PC {num}"))
         performance = int(st.text_input(label=f"enter performance of PC {num}"))
-        pcs = name,price,performance
-        st.dataframe(pcs)
+
+        pcs_dict[f"PC {num}"] = {'name' : name , 'price' : price , 'performance' : performance}
 except ValueError:
     pass
+try:
+    df=pd.DataFrame(pcs_dict)
+    print(df)
+    price_to_performance=st.write(df)
+except NameError:
+    pass
 
+st.button(label="Convert")
 
 
 print(st.session_state)
