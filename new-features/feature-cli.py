7f44cbd0c44979a7f41ea272184c539aecf26a33
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-05 22:11:08 +0530

    added search from database as a feature in basic form. Additionaly , now user can bifurcate between diff brands from data.csv

---
+++ new-features/feature-cli.py
@@ -0,0 +1,31 @@
+import csv
+import pandas as pd
+
+file = 'data.csv'
+df = pd.read_csv(file, usecols=['name','price','performance'])
+
+num_of_pcs = int(input("How many pcs to compare: "))
+
+pc_choice = input("Enter 1 for search and 2 for input: ")
+
+match pc_choice:
+    case '1':
+        print(df)
+        pc_brand = input("\nEnter 1 for Intel and 2 for AMD: ")
+        if pc_brand == '1' :
+            for name in (df['name']):
+                filtered_data = df[df['name'].str.startswith('Intel')]
+                filtered_data = filtered_data.reset_index(drop=True)
+                filtered_data.index = filtered_data.index + 1
+                print(filtered_data)
+                break
+        else:
+            for name in (df['name']):
+                filtered_data = df[df['name'].str.startswith('AMD')]
+                filtered_data = filtered_data.reset_index(drop=True)
+                filtered_data.index = filtered_data.index + 1
+                print(filtered_data)            
+                break
+    
+    case _:
+        print("Enter Valid Option")
