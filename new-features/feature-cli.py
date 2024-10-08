c5ed6ea32e75dd3d80a97202896a0cb5c815b2b5
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-09 00:52:36 +0530

    added choose from database and your choices get stored in avariable. The variable is printed to show the choices of the user. Next i have to work on new-feature to add integration with function ptp_calculator.

--- new-features/feature-cli.py
+++ new-features/feature-cli.py
@@ -1,41 +1,47 @@
 import csv
 import pandas as pd
 
-file = 'data.csv'
+file = 'pcs.csv'
 df = pd.read_csv(file, usecols=['name','price','performance'])
+
+user_choice = []
 
 num_of_pcs = int(input("How many pcs to compare: "))
 
-pc_choice = input("Enter 1 for search and 2 for input: ")
+for num in range(num_of_pcs):
+    pc_choice = input("Enter 1 for search and 2 for input: ")
 
-match pc_choice:
-    case '1':
-        print(df)
-        pc_brand = input("\nEnter 1 for Intel and 2 for AMD: ")
-        if pc_brand == '1' :
-            for name in (df['name']):
-                filtered_data = df[df['name'].str.startswith('Intel')]
-                filtered_data = filtered_data.reset_index(drop=True)
-                filtered_data.index = filtered_data.index + 1
+    match pc_choice:
+        case '1':
+            print(df)
+            pc_brand = input("\nEnter 1 for Intel and 2 for AMD: ")
+            if pc_brand == '1' :
+                for name in (df['name']):
+                    filtered_data = df[df['name'].str.startswith('Intel')]
+                    filtered_data = filtered_data.reset_index(drop=True)
+                    filtered_data.index = filtered_data.index + 1
                 print(filtered_data)
                 selected_index = int(input("\nEnter the number of the PC you want to select from the list: "))
                 selected_pc = filtered_data.iloc[selected_index-1].to_dict()
+                user_choice.append(selected_pc)
                 for key,value in selected_pc.items():
                     print(f"{key} : {value}")
-                break
-        elif pc_brand == '2':
-            for name in (df['name']):
-                filtered_data = df[df['name'].str.startswith('AMD')]
-                filtered_data = filtered_data.reset_index(drop=True)
-                filtered_data.index = filtered_data.index + 1
+            elif pc_brand == '2':
+                for name in (df['name']):
+                    filtered_data = df[df['name'].str.startswith('AMD')]
+                    filtered_data = filtered_data.reset_index(drop=True)
+                    filtered_data.index = filtered_data.index + 1
                 print(filtered_data) 
                 selected_index = int(input("\nEnter the number of the PC you want to select from the list: "))
                 selected_pc = filtered_data.iloc[selected_index-1].to_dict()
+                user_choice.append(selected_pc)
                 for key,value in selected_pc.items():
                     print(f"{key} : {value}")           
-                break
-        else:
-            print("Choose from above options")
-            
-    case _:
-        print("Enter Valid Option")
+            else:
+                print("Choose from above options")
+                continue
+        case _:
+            print("Enter Valid Option")
+            continue
+
+print(user_choice)
